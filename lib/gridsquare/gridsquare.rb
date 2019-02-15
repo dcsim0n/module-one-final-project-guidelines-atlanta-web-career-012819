8ac5e632cb1bab8fd44cd79aeb8f7f88c864518c
Dana Simmons <dana@simmons-audio.com>
2019-02-15 13:18:50 -0500

    added some comments

--- lib/gridsquare/gridsquare.rb
+++ lib/gridsquare/gridsquare.rb
@@ -1,18 +1,20 @@
 class Gridsquare
-  include Geokit::Mappable
   ALPHABET = ('A'..'S').to_a
 
   attr_reader :coordinate, :grid
-  def initialize(grid_string:)
+  def initialize(grid:)
     @grid = grid_string
     lat,lng = Gridsquare.grid_string_to_lat_long(grid_string)
     @coordinate = Geokit::LatLng.new(lat,lng)
   end
-  
+
   def self.alpha_to_int(alpha)
     ALPHABET.find_index(alpha)
   end
-
+  def distance_to_grid(grid) #take another grid opbjec and calculate the distance.
+    other_point = grid.coordinate
+    self.coordinate.distance_to(other_point).to_i
+  end
   def self.calc_longitude(msb:, lsb:)
     longitude = msb * 10
     longitude = longitude + lsb
@@ -27,6 +29,10 @@ class Gridsquare
     latitude = latitude - 90
   end
   def self.grid_string_to_lat_long(grid_string)
+    #Accepts a four character grid refference and converts in too
+    #Latitude and Longitude
+    # !!FIX ME!!  #
+    #Six character strings would be more precise, we will get there.
     longMSB = alpha_to_int(grid_string[0])
     longLSB = grid_string[2].to_i
 
